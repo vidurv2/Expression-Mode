d486f47fe2504c532ecdf7db8690934f360e6a6e
vidurveerduggal <vidurveer.duggal@gmail.com>
2020-11-13 18:19:27 +0800

    Modified output display style

--- Output Display/showBoxandEmotion.py
+++ Output Display/showBoxandEmotion.py
@@ -3,28 +3,22 @@ from cv2 import cv2
 import json
 import operator
 
-#Import json output from API 
 with open('test-result.json','r') as f:
     result = json.load(f)
-    
-#Number of faces detected 
+
 num_faces = len(result)
-
-#Import frame
 img = cv2.imread("test_img.jpeg",1)
-
 for i in range(num_faces):
-    #Draw bounding boxes
     start_x = result[i]["faceRectangle"]["left"]
     start_y = result[i]["faceRectangle"]["top"]
     end_x = start_x + result[i]["faceRectangle"]["width"]
     end_y = start_y + result[i]["faceRectangle"]["height"]
-    cv2.rectangle(img,(start_x,start_y),(end_x,end_y),(255,0,0))
-    
-    #Display text of emotion with highest score
+    cv2.rectangle(img,(start_x,start_y),(end_x,end_y),(0,0,255),2)
     emotions_dict = result[i]["faceAttributes"]["emotion"]
     highest_emotion = max(emotions_dict.items(),key=operator.itemgetter(1))[0]
-    cv2.putText(img , highest_emotion,(start_x-5,start_y-5),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255), 2)
+    cv2.rectangle(img,(start_x,end_y),(end_x,end_y+25),(0,0,255),cv2.FILLED)
+    cv2.putText(img,highest_emotion,(start_x,end_y+13),cv2.FONT_HERSHEY_DUPLEX,0.5,(255,255,255),1)
+    # cv2.putText(img , highest_emotion,(start_x-5,start_y-5),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255), 2)
     
 
 cv2.imshow("Image",img)
